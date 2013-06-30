f62f0302ac044b4e2e1602f99ced90c4b2c91e11
Aaron Jorbin <aaron@jorb.in>
2013-06-30 15:12:01 -0400

    If you're not embarresed by version 1, you shipped to late

---
+++ README.md
@@ -0,0 +1,7 @@
+# Xerography Pepercorn Saison
+
+Xerography Peppercorn Saison is a them for Xerography. In fact, it's the default theme.  It's still new and probobly sucks.  
+
+## Why Peppercorn Saison?
+
+Peppercorn Saison is a delicous beer by [Three Stars Brewing](http://www.threestarsbrewing.com/).
---
+++ index.js
@@ -0,0 +1,13 @@
+var peppercorn = function(){
+    this.author = {
+        "name" : "Aaron Jorbin",
+        "url" : "http://aaron.jorb.in",
+        "email": "aaron@jorb.in"
+    };
+
+    this.name = "Peppercorn Saison";
+    this.css = ["style.css"]
+    this.img = ["retina_dust.png"];
+    this.headerjs = ["prefixfree.min.js"];
+}
+module.exports = new peppercorn();
